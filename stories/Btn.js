967c378820bec521f482e609f5fac734d1b42a5f
Kitamura <hinitial@gmail.com>
2021-07-04 11:47:51 +0900

    Storybook for html, JavaScriptの処理を差し込む

--- stories/Btn.js
+++ stories/Btn.js
@@ -1,4 +1,6 @@
 import './assets/scss/_btn.scss';
+import copyCodeBlock from '@pickra/copy-code-block';
+import ScriptTemplete from './btn.html'
 
 export const createBtn = ({
   no_link = false,
@@ -6,13 +8,19 @@ export const createBtn = ({
   label,
   onClick,
 }) => {
+  const ret = document.createElement('div');
   const btn = document.createElement('p');
   let a;
   if(!no_link){
     a = document.createElement('a')
     a.href="#";
     // a のリンクで遷移しないようにする
-    a.addEventListener('click', e => {e.preventDefault();return onClick(e);});
+    a.addEventListener('click', e => {
+      e.preventDefault();
+      console.log(vm_test);
+          vm_test.show();
+      return onClick(e);
+    });
   }else{
     a = document.createElement('span');
     a.className = 'btn-no_link';
@@ -20,13 +28,15 @@ export const createBtn = ({
   a.innerText = label;
   btn.appendChild(a);
 
-
+  ret.insertAdjacentHTML('beforeend', ScriptTemplete);
 
   const class_name_ary = ['btn'];
   if(no_link){
     class_name_ary.push('is-disable');
   }
   btn.className = class_name_ary.join(' ');
+  ret.appendChild(btn);
+  ret.appendChild( copyCodeBlock(btn.innerHTML,{ shouldReturnDomEl: true}));
 
-  return btn;
+  return ret;
 };
--- stories/Btn.stories.js
+++ stories/Btn.stories.js
@@ -16,7 +16,7 @@ export default {
       control: { type: 'select', options: ['small', 'medium', 'large'] },
     },
     onClick: { action: 'onClick' },
-  },
+  }
 };
 
 const Template = ({ label, ...args }) => {
